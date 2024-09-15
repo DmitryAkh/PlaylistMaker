b82502fe1becfe924e8be8db13f811900491ecc4
DmitryAkh <akhabanin@yandex.ru>
2024-09-15 22:57:59 +0300

    Выполнены все задачи по 11 спринту

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.glide)
     annotationProcessor(libs.glide.compiler)
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
