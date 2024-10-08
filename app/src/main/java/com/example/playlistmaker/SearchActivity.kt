fcdd2431a4799493f934915f56a408b2721ce0d6
DmitryAkh <akhabanin@yandex.ru>
2024-10-08 18:06:31 +0300

    Выполнены задачи 12 спринта

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -81,6 +81,7 @@ class SearchActivity : AppCompatActivity() {
             tracks.clear()
             adapter.notifyDataSetChanged()
             hideKeyboard()
+            showHistoryList()
         }
 
         binding.backButton.setOnClickListener() {
