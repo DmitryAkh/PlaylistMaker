2463f78450ec1d38e86d62a6cca4eaaf45d232f9
DmitryAkh <akhabanin@yandex.ru>
2025-01-15 09:48:01 +0300

    Выполнены задачи 16 спринта

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
@@ -11,9 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
-import com.example.playlistmaker.creator.Creator
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
-import com.example.playlistmaker.player.domain.PlayerInteractor
 import com.example.playlistmaker.player.domain.PlayerState
 import com.example.playlistmaker.search.domain.models.Track
 import com.example.playlistmaker.util.Utils
